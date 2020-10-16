e42dda15283e956b5e5d2ed82a279e76c101485b
Arturo B Martín <arturobm776@gmail.com>
2020-10-16 12:34:14 +0200

    first commit

---
+++ views/index.html.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="/css/master.css">
+    <title>THE EVENT</title>
+  </head>
+  <body>
+    <?php include 'header.html.php' ?>
+    <main>
+      <article> <!-- Title -->
+        <h1> THE EVENT </h1>
+      </article>
+      <article> <!-- Information -->
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+        culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </article>
+    </main>
+    <?php include 'footer.html.php' ?>
+  </body>
+</html>
